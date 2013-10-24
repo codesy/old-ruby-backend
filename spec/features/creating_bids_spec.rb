b2338625f587722000a3657b93fb4059da5e5872
Edward Cho <zerokarmaleft@gmail.com>
2013-10-24 14:25:57 -0500

    fix crappy assert in spec for creating bids

--- spec/features/creating_bids_spec.rb
+++ spec/features/creating_bids_spec.rb
@@ -5,8 +5,7 @@ feature "Creating Bids" do
     user = FactoryGirl.create(:user)
 
     visit '/bids/new'
-    expect(page).to have_content('Sign in')
-    expect(page).to have_content('Sign up')
+    expect(page).to have_content('You need to sign in or sign up before continuing.')
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
     click_button 'Sign in'

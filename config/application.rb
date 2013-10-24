07c5832c0182bff4af8db126e958bc12a5ddc3ed
Edward Cho <zerokarmaleft@gmail.com>
2013-10-23 21:59:40 -0500

    make the bid form less fugly

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,6 @@ module Patronage
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.assets.path << Rails.root + '/vendor/assets/fonts'
+    config.assets.paths << Rails.root + '/vendor/assets/fonts'
   end
 end

758f2cfddef8599fbd891b7636eef91789f4144f
Edward Cho <zerokarmaleft@gmail.com>
2013-11-02 20:45:13 -0500

    add basic validation to bid creation

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,3 +1,13 @@
 class Bid < ActiveRecord::Base
   belongs_to :user
+
+  validates :url, presence: true
+
+  validates :offer,
+    presence:     true,
+    numericality: { greater_than_or_equal_to: 0 }
+
+  validates :ask,
+    presence:     true,
+    numericality: { greater_than_or_equal_to: 0 }
 end
--- spec/models/bid_spec.rb
+++ spec/models/bid_spec.rb
@@ -2,4 +2,11 @@ require 'spec_helper'
 
 describe Bid do
   it { should belong_to(:user) }
+  it { should validate_presence_of(:url) }
+
+  it { should validate_presence_of(:offer) }
+  it { should validate_numericality_of(:offer).is_greater_than_or_equal_to(0) }
+
+  it { should validate_presence_of(:ask) }
+  it { should validate_numericality_of(:ask).is_greater_than_or_equal_to(0) }
 end
